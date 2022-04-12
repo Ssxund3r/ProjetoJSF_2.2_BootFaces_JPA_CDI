ad173b2b446dcbaada43fe619072fa3e87bddbda
Ssxund3r <gabrielfillip@gmail.com>
2022-04-12 18:13:27 -0300

    Backing bean componentes html jsf

--- src/main/java/br/com/ProjetoJsf/PessoaBean.java
+++ src/main/java/br/com/ProjetoJsf/PessoaBean.java
@@ -3,14 +3,17 @@ package br.com.ProjetoJsf;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
-@ApplicationScoped
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 	private String nome;
 	private List<String> nomes = new ArrayList<String>();
+
+	private HtmlCommandButton commandButton;
 
 	public String getNome() {
 		return nome;
@@ -28,8 +31,21 @@ public class PessoaBean {
 		this.nomes = nomes;
 	}
 
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+
 	public String addNome() {
 		nomes.add(nome);
+
+		if (nomes.size() > 2) {
+			commandButton.setDisabled(true);
+		}
+
 		return "";
 	}
 
