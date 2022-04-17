1e8df2e758ffaae6968de11a2bcbb49d703a195a
Ssxund3r <gabrielfillip@gmail.com>
2022-04-17 01:06:59 -0300

    Atualizando correção de inconsistências

--- src/main/java/br/com/ProjetoJsf/PessoaBean.java
+++ src/main/java/br/com/ProjetoJsf/PessoaBean.java
@@ -1,11 +1,7 @@
 package br.com.ProjetoJsf;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.component.html.HtmlCommandButton;
 
 import br.com.dao.GenericDao;
 import br.com.entidades.Pessoa;
