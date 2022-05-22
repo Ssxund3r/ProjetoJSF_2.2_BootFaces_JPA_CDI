c82c5dc3d18bec3e46d34a53a6cf6a8606a5b995
Ssxund3r <gabrielfillip@gmail.com>
2022-05-22 11:26:38 -0300

    Criando a camada de modelo para upload

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -71,11 +75,42 @@ public class Pessoa implements Serializable {
 
 	@Transient // Não fica persistente ou não grava no banco de dados...
 	private Estados estados;
-	
+
+	@Column(columnDefinition = "text") // Tipo text grava arquivos em base64
 	private String fotoIconBase64;
+
+	private String extensao; // extensão jpg, png, jpeg...
+
+	@Lob // Gravar arquivos no banco de dados
+	@Basic(fetch = FetchType.LAZY)
+	private byte[] fotoIconBase64Original;
 
 	public Pessoa() {
 
+	}
+
+	public String getExtensao() {
+		return extensao;
+	}
+
+	public void setExtensao(String extensao) {
+		this.extensao = extensao;
+	}
+
+	public byte[] getFotoIconBase64Original() {
+		return fotoIconBase64Original;
+	}
+
+	public void setFotoIconBase64Original(byte[] fotoIconBase64Original) {
+		this.fotoIconBase64Original = fotoIconBase64Original;
+	}
+
+	public String getFotoIconBase64() {
+		return fotoIconBase64;
+	}
+
+	public void setFotoIconBase64(String fotoIconBase64) {
+		this.fotoIconBase64 = fotoIconBase64;
 	}
 
 	public Cidades getCidades() {
