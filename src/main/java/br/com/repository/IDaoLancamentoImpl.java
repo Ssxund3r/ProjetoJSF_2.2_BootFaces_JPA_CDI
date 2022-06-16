a8328c993db067a92212c79bad6dcb0f9d7efe1e
Ssxund3r <gabrielfillip@gmail.com>
2022-06-16 17:19:56 -0300

    Criando relatório com intervalo de datas

--- src/main/java/br/com/repository/IDaoLancamentoImpl.java
+++ src/main/java/br/com/repository/IDaoLancamentoImpl.java
@@ -1,6 +1,7 @@
 package br.com.repository;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -54,22 +55,50 @@ public class IDaoLancamentoImpl implements IDaoLancamento, Serializable {
 	@Override
 	public List<Lancamento> relatorioLancamento(String numNota, Date dataIni, Date dataFim) {
 		List<Lancamento> lancamentos = new ArrayList<Lancamento>();
-		
+
 		StringBuilder sql = new StringBuilder();
 
 		sql.append(" select l from Lancamento l ");
 
 		if (dataIni == null && dataFim == null && numNota != null && !numNota.isEmpty()) {
+
 			sql.append(" where l.numeroNotaFiscal = '").append(numNota.trim()).append("'");
+
+		} else if (numNota == null || (numNota != null && numNota.isEmpty()) && dataIni != null && dataFim == null) {
+
+			String dataIniString = new SimpleDateFormat("yyyy-MM-dd").format(dataIni);
+			sql.append(" where l.dataIni >= '").append(dataIniString).append("'");
+
+		} else if (numNota == null || (numNota != null && numNota.isEmpty()) && dataIni == null && dataFim != null) {
+
+			String dataFimString = new SimpleDateFormat("yyyy-MM-dd").format(dataFim);
+			sql.append(" where l.dataFinal <= '").append(dataFimString).append("'");
+
+		} else if (numNota == null || (numNota != null && numNota.isEmpty()) && dataIni != null && dataFim != null) {
+
+			String dataIniString = new SimpleDateFormat("yyyy-MM-dd").format(dataIni);
+			String dataFimString = new SimpleDateFormat("yyyy-MM-dd").format(dataFim);
+
+			sql.append(" where l.dataIni >= '").append(dataIniString).append("' ");
+			sql.append(" and l.dataFinal <= '").append(dataFimString).append("' ");
+
+		} else if (numNota != null && !numNota.isEmpty() && dataIni != null && dataFim != null) {
+
+			String dataIniString = new SimpleDateFormat("yyyy-MM-dd").format(dataIni);
+			String dataFimString = new SimpleDateFormat("yyyy-MM-dd").format(dataFim);
+
+			sql.append(" where l.dataIni >= '").append(dataIniString).append("' ");
+			sql.append(" and l.dataFinal <= '").append(dataFimString).append("' ");
+			sql.append(" and l.numeroNotaFiscal = '").append(numNota.trim()).append("'");
 		}
 
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
-		
+
 		lancamentos = entityManager.createQuery(sql.toString()).getResultList();
-		
+
 		transaction.commit();
-		
+
 		return lancamentos;
 	}
 
